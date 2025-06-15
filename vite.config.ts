448ee1b5cb18ab3544705bd431a0c5d1c972c7a1
lucian diaconu <luciandiaconu123@gmail.com>
2025-06-15 18:18:38 +0200

    fix: configure styled-components as the styled engine for MUI in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Use styled-components as the styled engine for MUI
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    },
+  },
+});
