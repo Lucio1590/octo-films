2bf02fcf20605ca8c1ff69ea9f12717532389663
lucian diaconu <lucian.diaconu@freseniusmedicalcare.com>
2025-06-17 22:05:22 +0200

    chore: set local port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       // Use styled-components as the styled engine for MUI
-      "@mui/styled-engine": "@mui/styled-engine-sc",
+      '@mui/styled-engine': '@mui/styled-engine-sc',
     },
   },
-});
+})
