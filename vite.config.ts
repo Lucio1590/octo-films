7b2affa61b15e2adc171133ff0ed6164de6d6d61
lucian diaconu <luciandiaconu123@gmail.com>
2025-06-18 01:07:44 +0200

    feat: navigation, and user data mock, theming

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Use styled-components as the styled engine for MUI
-      "@mui/styled-engine": "@mui/styled-engine-sc",
+      '@mui/styled-engine': '@mui/styled-engine-sc',
     },
   },
-});
+})
